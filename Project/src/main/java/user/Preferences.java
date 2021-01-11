5fe61d2cb6bcc8c6fa8e7ae13cc2014afae71998
Łukasz Wieczorkiewicz <lwieczorkiewicz@gmail.com>
2021-01-11 13:16:32 +0100

    Adding files for class User

---
+++ Project/src/main/java/user/Preferences.java
@@ -0,0 +1,12 @@
+package user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Preferences {
+    List<String> alergy = new ArrayList<>();
+    List<String> dietLabel = new ArrayList<>();
+    private int countMealsPerDay;
+    private int countColouriesPerDay;
+
+}
---
+++ Project/src/main/java/user/User.java
@@ -0,0 +1,53 @@
+package user;
+
+public class User {
+
+    private String name;
+    private Gender gender;
+    private int age;
+    private Preferences preferences;
+
+
+    public User() {
+    }
+
+    public User(String name, Gender gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Preferences getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(Preferences preferences) {
+        this.preferences = preferences;
+    }
+}
