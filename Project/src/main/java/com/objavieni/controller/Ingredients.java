c831029342906f75e16b3a44f661ffbbd58b3637
Paweł Korniak <pawello1020@gmail.com>
2021-01-28 22:45:47 +0100

    getting ingredients works witch checkboxes

---
+++ Project/src/main/java/com/objavieni/controller/Ingredients.java
@@ -0,0 +1,2 @@
+package com.objavieni.controller;public class Ingredients {
+}
---
+++ Project/src/test/java/com/objavieni/mealDistribution/MealDistributorTest.java
@@ -0,0 +1,6 @@
+package com.objavieni.mealDistribution;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MealDistributorTest {
+  
+}
